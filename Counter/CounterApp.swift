37d23721dd400b44854d3e045a65a5a0c65a5361
Nathyane Moreno <nathyane.moreno@gmail.com>
2025-02-12 18:28:09 -0300

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Nathyane Moreno on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

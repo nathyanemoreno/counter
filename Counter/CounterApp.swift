d348e27652d517dea9108111e9237d5a5856e85e
Nathyane Moreno <nathyane.moreno@gmail.com>
2025-02-21 09:59:34 -0300

    feat: add Counter logic and UI

--- Counter/CounterApp.swift
+++ Counter/CounterApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct CounterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([Counter.self])
+        let configuration = ModelConfiguration()
+        return try! ModelContainer(for: schema, configurations: [configuration])
+    }()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let counterRepository = CounterRepository(modelContext: sharedModelContainer.mainContext)
+            let viewModel = CounterViewModel(counterRepository: counterRepository)
+            
+            ContentView(viewModel: viewModel)
+                .modelContainer(sharedModelContainer)
+                .environment(\.locale, Locale(identifier: "pt_BR"))
+                .environment(\.font, Font.custom("Avenir", size: 14))
         }
     }
 }
